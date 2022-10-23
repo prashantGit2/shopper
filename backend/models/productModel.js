9234ccb2a1fce54d369a4c38187a0fa371c5e757
Prashant Sharma <87496363+prashantGit2@users.noreply.github.com>
2022-10-24 03:08:20 +0530

    role authentication added

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -57,6 +57,12 @@ const productShema = new mongoose.Schema({
             required:true
         }
     }],
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:"User",
+        required:true
+
+    },
     createdAt:{
         type:Date,
         default:Date.now()
