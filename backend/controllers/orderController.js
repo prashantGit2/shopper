f45b1681c12d3d9772b0e213967600ae7a018b1b
Prashant Sharma <87496363+prashantGit2@users.noreply.github.com>
2023-02-27 19:43:39 +0530

    single order api and user model issue fixed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -32,3 +32,25 @@ exports.newOrder = catchAsyncError(async (req, res, next) => {
     order,
   })
 });
+
+// Get Single Order
+exports.getSingleOrder = catchAsyncError(async (req,res,next) => {
+  // const order  = await Order.findById(req.params.id).populate("user", "name email")
+  const order  = await Order.findById(req.params.id).populate("user",["name","email"])
+  if (!order) {
+    return next(new ErrorHandler("No order found with that ID", 404))
+  }
+  res.status(200).json({
+    success:true,
+    order,
+  })
+})
+
+// Get My Orders
+exports.getMyOrders = catchAsyncError(async (req,res,next) => {
+  const orders  = await Order.find({user: req.user._id})
+  res.status(200).json({
+    success:true,
+    orders,
+  })
+})
